b75478b049c8968f4a296e175b90e738fca3275f
Antonio Rodriguez Farias <abgrodriguezfarias@gmail.com>
2025-09-20 23:37:15 +0200

    Update firestore_admin.py

--- firestore_admin.py
+++ firestore_admin.py
@@ -2,7 +2,7 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
 
-
+ 
 cred = credentials.Certificate("C:\CyberGuardian\serviceAccountKey.json")
 firebase_admin.initialize_app(cred)
 db = firestore.client()
